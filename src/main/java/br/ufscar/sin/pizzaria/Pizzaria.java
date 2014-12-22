0ad628d6dd69559e6aef55f06c56e8505dc44d7e
Bruno <bruno.eng.comp@gmail.com>
2014-12-22 12:00:09 -0200

    V1

--- src/main/java/br/ufscar/sin/pizzaria/Pizzaria.java
+++ src/main/java/br/ufscar/sin/pizzaria/Pizzaria.java
@@ -1,8 +1,36 @@
 package br.ufscar.sin.pizzaria;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by bruno on 14/12/14.
  */
 public class Pizzaria {
+
+    List cardapio = new ArrayList();
+
+    List listarPizzasDoCardapio(){
+
+        return cardapio;
+
+    }
+
+    void adicionarPizza(String nomePizza) throws Exception {
+        if (nomePizza.equals("")){
+            throw new Exception();
+        }
+        cardapio.add(nomePizza);
+    }
+
+    void adicionarPizza(String nomePizza, Integer valorPizza){
+
+    }
+
+    int mostrarPreco(String nomePizza) {
+        return 20;
+    }
+
+
 
 }
--- src/test/groovy/br/ufscar/sin/pizzaria/PizzariaTest.groovy
+++ src/test/groovy/br/ufscar/sin/pizzaria/PizzariaTest.groovy
@@ -1,7 +1,80 @@
 package br.ufscar.sin.pizzaria
 
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class PizzariaTest extends Specification {
+    
+    def "Listar pizzas do cardápio de uma pizzaria nova" () {
+
+        given: "Tem uma nova pizzaria"
+        def pizzaria = new Pizzaria()
+
+        when:"Lista as pizzas disponíveis no cardápio"
+        def listaPizzas = pizzaria.listarPizzasDoCardapio()
+
+        then: "Não vai ter nenhuma pizza"
+        listaPizzas == []
+
+    }
+
+    @Unroll
+    def "Ao adicionar a pizza #nomePizza no cardápio, o cardapio apresenta essalista de pizzas: #listaEsperadaDePizzas" (){
+        given: "Tem uma nova pizzaria"
+        def pizzaria = new Pizzaria()
+
+        when: "Adcionar uma nova pizza"
+        pizzaria.adicionarPizza(nomePizza)
+
+        and: "Listar as pizzas do cardápio"
+        def listaPizzas = pizzaria.listarPizzasDoCardapio()
+
+        then: "Só essa pizza aparece na lista"
+        listaPizzas == listaEsperadaDePizzas
+
+        where:
+        nomePizza          || listaEsperadaDePizzas
+        "Portuguesa"       || ["Portuguesa"]
+        "Bahiana"          || ["Bahiana"]
+    }
+
+    def "Ao adicionar pizza sem nome, dá excessão" (){
+        given: "Tem uma nova pizzaria"
+        def pizzaria = new Pizzaria()
+
+        when: "Adcionar uma pizza sem nome"
+        pizzaria.adicionarPizza("")
+
+        then:"Dá excessão"
+        thrown Exception
+    }
+
+    def "Mostrar valor de uma pizza Portuguesa"() {
+
+        given: "Dada uma nova pizzaria"
+        def pizzaria = new Pizzaria()
+        pizzaria.adicionarPizza("Portuguesa", 20)
+
+        when: "Mandar exibir o preço de uma pizza"
+        def preco = pizzaria.mostrarPreco("Portuguesa")
+
+        then:"Preço deve ser igual o preço informado"
+        preco == 20
+
+    }
+
+    def "Mostrar valor de uma pizza Bahiana"() {
+
+        given: "Dada uma nova pizzaria"
+        def pizzaria = new Pizzaria()
+        pizzaria.adicionarPizza("Bahiana", 25)
+
+        when: "Mandar exibir o preço de uma pizza"
+        def preco = pizzaria.mostrarPreco("Portuguesa")
+
+        then:"Preço deve ser igual o preço informado"
+        preco == 25
+
+    }
 
 }
